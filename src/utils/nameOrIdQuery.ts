483e621027cc6ad9eca25fcf1fab31cba94ab14e
Tomasz Prazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-28 13:55:02 +0100

    Update nameOrIdQuery.ts

--- src/utils/nameOrIdQuery.ts
+++ src/utils/nameOrIdQuery.ts
@@ -1,9 +1,7 @@
 const ObjectId = require('mongoose').Types.ObjectId;
 export const nameOrIdQuery = (idOrName) => {
   if (ObjectId.isValid(idOrName)) {
-    console.log('yhy');
     if (String(new ObjectId(idOrName)) === idOrName) {
-      console.log('weszlo');
       return { _id: idOrName };
     }
   }
